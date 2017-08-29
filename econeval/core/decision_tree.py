61fecd45d5fc4374111dbc3eb492db8200674704
Hans Olav Melberg <hans.melberg@gmail.com>
2017-08-29 17:08:46 +0200

    decison tree class addd

--- econeval/core/decision_tree.py
+++ econeval/core/decision_tree.py
@@ -72,16 +72,10 @@ class DecisionTree(object):
         nxg: a network graph 
            
         """
-        self.add_many_states(nxg.nodes(data=True))
+        #self.add_many_states(nxg.nodes(data=True))
         self.add_many_edges(nxg.edges(data=True))   
        
-    def _to_nx_state(self):
-        statelist = []
-        for state in self.state:
-            info=(state, self.state[state])
-            statelist.append(info)
-        return statelist
-        
+       
     def _to_nx_edge(self):    
         edgelist = []
         for from_state in self.edge:
@@ -91,63 +85,13 @@ class DecisionTree(object):
         return edgelist
     
     def plot(self):
-        states = self._to_nx_state()
         edges = self._to_nx_edge()
         g = nx.DiGraph()
-        g.add_nodes_from(states)
         g.add_edges_from(edges)
             
         return draw(g, format = 'svg')
         
-    def add_many_states(self, state, attr=None, order=None):
-        """
-            Adds multiple states and associated attributes to the model
-            
-            Example
-            -------
-            states = [('healthy', {'u': 0.8, 'c':0}), 
-                      ('dead',  {'u':0, 'c':0}),
-                      ('sick', {'u':0.2, 'c':1000})]
-            
-            testmodel.add_many_states(states)
-        """
-        
-        self.state.update(dict(state))
-        self.states.append(list(self.state.keys()))
-          
-    def add_state(self, state, attr=None, order=None):
-        """
-        Adds a node with some optinal attributes (in a dictionary)
-        
-        state: string
-            label of node
-        
-        attr: dict
-            attributes of the node
-            
-        Example
-        -------
-        hipmodel.add_state('very sick', {'u':0.1, 'c':5000}) 
-        """
-        self.state.update({state: attr})
-        self.states.append(state)
-            
-    def del_state(self, state):
-        """
-        Deletes a state
-                        
-        state: string
-            label of node
-        """
-        #todo: update connections automatically after deleting a state?
-        #in which case: default rule for distributing probability?
-        
-        for state in self.states:
-            if st[0]==state:
-                self.states.remove(st)
-                break
-            print('Error: State not found')
-    
+       
     def add_edge(self, from_state, to_state, attr):
         """
         Adds connections between states and give the connection attributes
@@ -195,21 +139,7 @@ class DecisionTree(object):
             self.edge[from_state] = {v[1]:v[2] for v in edges if from_state==v[0]}
         self.pmatrix=self.np_matrix()
     
-    def np_matrix(self):
-        matrix=[]
-        states = self.state.keys()
-        for from_state in states:
-            plist=[]
-            for to_state in states:
-                print(from_state, to_state)
-                try: 
-                    p = self.edge[from_state][to_state]['p']
-                except:
-                    p = 0
-                plist.append(p)
-            matrix.append(plist)
-        return matrix
-    
+   
 
     def expected(self, var):
         g=self.to_nx()
@@ -270,10 +200,8 @@ class DecisionTree(object):
         return df
     
     def to_nx(self):
-        nodes = self._to_nx_state()
-        edges = self._to_nx_edge()
+        edges = self._to_nx_edge() 
         g=nx.DiGraph()
-        g.add_nodes_from(nodes)
         g.add_edges_from(edges)
         return g
     
@@ -324,39 +252,39 @@ class DecisionTree(object):
 
 
 #%% liver diagnostic
-
-states = [('susceptible',           {'u':lookup_csv(dfs, valuevar='p_die_stI', attr=['Age']), 'c':100}),
-          ('low_fibrosis_index',    {'u':0.5, 'c':100}),
-          ('high_fibrosis_index',   {'u':0.5, 'c':100}),          
-          ('advanced_fibrosis',     {'u':0.4, 'c':1000}),
-          ('no_advanced_fibrosis',  {'u':0.4, 'c':1000}),
-          ('low_elf',               {'u':0.4, 'c':1000}),
-          ('high_elf',              {'u':0.4, 'c':1000}),
-          ('low_ela',               {'u':0.4, 'c':1000}),
-          ('high_ela',              {'u':0.4, 'c':1000}),
-          ('quit_drink',            {'u':0.4, 'c':1000}),
-          ('dead',                  {'u':0.4, 'c':1000})]      
-
-#adding edges between states (with probabilities)
-transitions = [
-    ('susceptible', 'low_fibrosis_index', {'p': 58/128}),
-    ('susceptible', 'high_fibrosis_index', {'p': 70/128}),
-    
-    ('low_fibrosis_index', 'susceptible', {'p': 50/58}),
-    ('low_fibrosis_index', 'quit_drink', {'p': 8/58}),
-         
-    ('high_fibrosis_index', 'low_elf', {'p': 60/70}),
-    ('high_fibrosis_index', 'high_elf', {'p': 10/70}),
-    
-    ('low_elf', 'susceptible', {'p': 45/60}),
-    ('low_elf', 'quit_drink', {'p': 15/60}),
-    
-    ('high_elf', 'low_ela', {'p': 0/10}),
-    ('high_elf', 'high_ela', {'p': 10/10}),
-    
-    ('low_ela', 'no_advanced_fibrosis', {'p': 0/10}),
-    ('low_ela', 'advanced_fibrosis', {'p': 10/10}),
-    
-    ('high_ela', 'no_advanced_fibrosis', {'p': 7/59}),
-    ('high_ela', 'advanced_fibrosis', {'p': 52/59})]
+#
+#states = [('susceptible',           {'u':lookup_csv(dfs, valuevar='p_die_stI', attr=['Age']), 'c':100}),
+#          ('low_fibrosis_index',    {'u':0.5, 'c':100}),
+#          ('high_fibrosis_index',   {'u':0.5, 'c':100}),          
+#          ('advanced_fibrosis',     {'u':0.4, 'c':1000}),
+#          ('no_advanced_fibrosis',  {'u':0.4, 'c':1000}),
+#          ('low_elf',               {'u':0.4, 'c':1000}),
+#          ('high_elf',              {'u':0.4, 'c':1000}),
+#          ('low_ela',               {'u':0.4, 'c':1000}),
+#          ('high_ela',              {'u':0.4, 'c':1000}),
+#          ('quit_drink',            {'u':0.4, 'c':1000}),
+#          ('dead',                  {'u':0.4, 'c':1000})]      
+#
+##adding edges between states (with probabilities)
+#transitions = [
+#    ('susceptible', 'low_fibrosis_index', {'p': 58/128}),
+#    ('susceptible', 'high_fibrosis_index', {'p': 70/128}),
+#    
+#    ('low_fibrosis_index', 'susceptible', {'p': 50/58}),
+#    ('low_fibrosis_index', 'quit_drink', {'p': 8/58}),
+#         
+#    ('high_fibrosis_index', 'low_elf', {'p': 60/70}),
+#    ('high_fibrosis_index', 'high_elf', {'p': 10/70}),
+#    
+#    ('low_elf', 'susceptible', {'p': 45/60}),
+#    ('low_elf', 'quit_drink', {'p': 15/60}),
+#    
+#    ('high_elf', 'low_ela', {'p': 0/10}),
+#    ('high_elf', 'high_ela', {'p': 10/10}),
+#    
+#    ('low_ela', 'no_advanced_fibrosis', {'p': 0/10}),
+#    ('low_ela', 'advanced_fibrosis', {'p': 10/10}),
+#    
+#    ('high_ela', 'no_advanced_fibrosis', {'p': 7/59}),
+#    ('high_ela', 'advanced_fibrosis', {'p': 52/59})]
 
